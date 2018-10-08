d63c02e2e72a5b9759d0cc06ed8b9931f41d23a2
mattvlaw <mattvlaw@gmail.com>
2018-10-08 11:56:11 -0400

    added a basic shell to ros.py that tests to see if the rosbridge has connected

--- distopia/app/ros.py
+++ distopia/app/ros.py
@@ -6,4 +6,18 @@ Publishes the state of the system to ROS.
 """
 
 import roslibpy
+import time
 
+class RosBridge:
+    """Maintains subscribers and publishers to a ROS master via rosbridge
+    """
+
+    def __init__(self,host="localhost",port=9090):
+        print("Attempting to connect to rosbridge at:{}:{}...".format(host,port) 
+        self.ros = roslibpy.Ros(host=host,port=port)
+        self.ros.on_ready(lambda: print('Is ROS connected?', self.ros.is_connected)
+        time.sleep(10) #for now, just wait 10 seconds to see if it connects
+        print("Shutting down")
+
+if __name__ == '__main__':
+    ros = RosBridge()
